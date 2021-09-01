24260d1c0531d4c47ebbd012ec5dec9d84837486
Chris De montigny <chrisdemontigny@yahoo.com>
2021-08-31 19:24:02 -0600

    authController skeleton

---
+++ nvp-data-structures/server/controllers/authController.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcrypt')
+
+module.exports = {
+    register: async (req,res) => {
+
+    },
+
+    login: async (req,res) => {
+
+    },
+
+    logout: async (req,res) => {
+        
+    }
+}
