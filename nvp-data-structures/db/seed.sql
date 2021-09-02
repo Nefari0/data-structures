dea3df22f33b7b729db0d3c62cc7ef6dc5e49b5f
Chris De montigny <chrisdemontigny@yahoo.com>
2021-09-01 23:47:17 -0600

    started building db seed

---
+++ nvp-data-structures/db/seed.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE nvp_user (
+--   user_id SERIAL PRIMARY KEY,
+--   first_name VARCHAR(250),
+--   last_name VARCHAR(250),
+--   email VARCHAR(250) NOT NULL,
+--   hash VARCHAR(30) NOT NULL
+-- )
+
+-- INSERT INTO nvp_user (first_name,last_name,email,hash)
+-- VALUES(
+--   'chris',
+--   'deMontigny',
+--   'my email',
+--   'pass'
+-- )
