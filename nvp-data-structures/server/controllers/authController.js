5fa11e16925858e4742d01c0ce5b7e737fbdf8c9
Chris De montigny <chrisdemontigny@yahoo.com>
2021-09-01 23:48:53 -0600

    started login and register controllers

--- nvp-data-structures/server/controllers/authController.js
+++ nvp-data-structures/server/controllers/authController.js
@@ -2,14 +2,49 @@ const bcrypt = require('bcrypt')
 
 module.exports = {
     register: async (req,res) => {
+        const { first_name,last_name,email,password } = req.body
+        const db = req.app.get('db')
+        const result = await db.auth.get_user([email]) 
+        const existingUser = result[0]
+        if (existingUser) {
+            return res.status(409).send('This email is being used by another user')
+        }
+        const salt = bcrypt.genSaltSync(10);
+        hash = bcrypt.hashSync(password,salt)
+        const registeredUser = await db.auth.register_user([first_name,last_name,email,password])
+        const user = registeredUser[0]
 
+        req.session.user = {
+            firstName:user.first_name,
+            lastName:user.last_name,
+            email:user.email,
+            id:user.id
+        }
+        return res.status(201).send(req.session.user).catch(err => console.log(err))
     },
 
     login: async (req,res) => {
+        const { email,password } = req.body
+        const foundUser = await req.app.get('db').get_user([email])
+        const user = foundUser[0];
+        if (!user) {
+            return res.status(401).send("user not found")
+        }
+        const isAuthenticated = bcrypt.compareSync(password,user.hash);
+        if (!isAuthenticated) {
+            return res.status(403).send("Incorrect Password")
+        }
 
+        req.session.user = {
+            email:user.email,
+            name:user.first_name,
+            id:user.id,
+            auth:isAuthenticated
+        }
+        return res.status(200).send(req.session.user)
     },
 
     logout: async (req,res) => {
-        
+
     }
 }
