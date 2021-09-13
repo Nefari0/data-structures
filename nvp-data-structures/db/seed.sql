de62f18308e3dbba62c92e8ca3bc01c8af6c0371
Chris De montigny <chrisdemontigny@yahoo.com>
2021-09-12 20:34:49 -0600

    create table that awaits machine learning processing and sample element

--- nvp-data-structures/db/seed.sql
+++ nvp-data-structures/db/seed.sql
@@ -27,3 +27,25 @@
 
 -- ---- cancer database/calculator ---- --
 -- database and calculator are generated in jupyter notebook
+
+-- pending calculation database --
+-- CREATE TABLE cancer_stats_pending (
+--   data_id SERIAL PRIMARY KEY,
+--   id INTEGER,
+--   clump_thickness INTEGER,
+--   uniformity_of_cell_size INTEGER,
+--   uniformity_of_cell_shape INTEGER,
+--   marginal_adhesion INTEGER,
+--   single_epithelial_cell_size INTEGER,
+--   bare_nuclei INTEGER,
+--   bland_chromatin INTEGER,
+--   normal_nuceoli INTEGER,
+--   mitoses INTEGER
+-- )
+
+-- INSERT INTO cancer_stats_pending (id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithelial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses)
+-- VALUES (
+--   '1000025','5','1','1','1','2','1','3','1','1'
+-- )
+
+-- select * from cancer_stats_pending where id = '1000025'
