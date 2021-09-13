239e169e21d454a64bb9aa16bf290eae5739d907
Chris De montigny <chrisdemontigny@yahoo.com>
2021-09-13 09:31:54 -0600

    latest changes

--- nvp-data-structures/server/controllers/cancerStatController.js
+++ nvp-data-structures/server/controllers/cancerStatController.js
@@ -8,7 +8,7 @@ module.exports = {
 
     addToDatabase: async (req,res) => {
         const { id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithelial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses } = req.body
-        console.log('id',id)
+        console.log('from controller',id)
         const db = req.app.get('db')
         newStat = await db.cancer.add_cancer([id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithelial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses])
         return res.status(200).send(newStat)
