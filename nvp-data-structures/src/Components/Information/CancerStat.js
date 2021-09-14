678170b02bf1024533fa0438d30f7b112b3d5ccb
Chris De montigny <chrisdemontigny@yahoo.com>
2021-09-13 18:57:47 -0600

    information form that sends new info to database pending prediction algorithm

--- nvp-data-structures/src/Components/Information/CancerStat.js
+++ nvp-data-structures/src/Components/Information/CancerStat.js
@@ -1,15 +1,25 @@
 import axios from 'axios'
 import react from 'react'
 import './Info.css'
+import './Column.css'
 
 const CancerStat = (props) => {
 
-    const { eclass,id } = props
+    const { eclass,id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithlial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses } = props
 
     return(
-        <div className="info-list">
-            <p className="p-text">{id}</p>
-            <p className="p-text">{eclass}</p>
+        <div className="scrollitems">
+            <a>{id}</a>
+            <a >{clump_thickness}</a>
+            <a>{uniformity_of_cell_size}</a>
+            <a>{uniformity_of_cell_shape}</a>
+            <a>{marginal_adhesion}</a>
+            <a>{single_epithlial_cell_size}</a>
+            <a>{bare_nuclei}</a>
+            <a>{bland_chromatin}</a>
+            <a>{normal_nuceoli}</a>
+            <a>{mitoses}</a>
+            <a>{eclass}</a>
         </div>
     )
 }
