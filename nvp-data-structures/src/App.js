ca0250f83486eefdff796839cda29233b062345b
Nefari0 <chrisdemontigny@yahoo.com>
2022-05-10 22:31:38 -0600

    pyscript tinker

--- nvp-data-structures/src/App.js
+++ nvp-data-structures/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Auth from './Components/Auth/Auth'
 import Header from './Components/Header/Header';
 import Info from './Components/Information/Info'
+// import Py from './Components/pytest/Py';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      {/* <Py/> */}
       <Auth/>
       {/* <Info/> */}
       {/* <header className="header"><h3>React</h3></header> */}
