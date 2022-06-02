33314842f16e5d36504196dd533a4689498b6068
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-01 22:24:27 -0600

    cleaning

--- nvp-data-structures/server/controllers/cancerStatController.js
+++ nvp-data-structures/server/controllers/cancerStatController.js
@@ -4,13 +4,11 @@ module.exports = {
         const db = req.app.get('db')
         const stats = await db.cancer.get_cancer_all()
         let totalNumber = stats.length
-        console.log('total number',totalNumber)
         return res.status(200).send(stats)
     },
 
     addToDatabase: async (req,res) => {
         const { id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithelial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses } = req.body
-        console.log('from controller',id)
         const db = req.app.get('db')
         newStat = await db.cancer.add_cancer([id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithelial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses])
 
@@ -22,7 +20,6 @@ module.exports = {
     },
 
     oneResult: async (req,res) => {
-        console.log('oneResult firing!')
         const db = req.app.get('db')
         const { id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithelial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses } = req.body
         sentOnfo = await db.cancer.add_cancer([id,clump_thickness,uniformity_of_cell_size,uniformity_of_cell_shape,marginal_adhesion,single_epithelial_cell_size,bare_nuclei,bland_chromatin,normal_nuceoli,mitoses])
