fb14f1669b5886e08b83155d330eb0bd145e0b3e
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-02 16:52:01 -0600

    cleaning

--- nvp-data-structures/src/App.js
+++ nvp-data-structures/src/App.js
@@ -1,24 +1,12 @@
-// import logo from './logo.svg';
 import './App.css';
 import Auth from './Components/Auth/Auth'
 import Header from './Components/Header/Header';
-import Info from './Components/Information/Info'
-// import Py from './Components/pytest/Py';
-// import './Components/pytest/Py.html'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Py/> */}
       <Auth/>
-      {/* <Info/> */}
-      {/* <header className="header"><h3>React</h3></header> */}
-      {/* <body> */}
-        {/* <h3>elements</h3> */}
-      {/* </body> */}
-
-      
     </div>
   );
 }
