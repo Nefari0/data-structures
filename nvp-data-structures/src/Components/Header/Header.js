01b5df94346979df45d71bef9a7c456195a52336
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-06 13:26:35 -0600

    clean up

--- nvp-data-structures/src/Components/Header/Header.js
+++ nvp-data-structures/src/Components/Header/Header.js
@@ -4,8 +4,6 @@ import image from '../logo.png'
 const Header = (props) => {
     return(
         <div className="header">
-            {/* <h3>header</h3> */}
-            {/* <img src="https://firebasestorage.googleapis.com/v0/b/depot-7bb3e.appspot.com/o/logo.jpeg?alt=media&token=9d6b52c0-738e-4c1f-bf26-8323cacac73d" className="logo" alt="NVP"/> */}
             <img src={image} className="logo" alt="CD"/>
         </div>
     )
