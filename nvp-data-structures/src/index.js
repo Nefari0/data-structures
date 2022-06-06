f49d4e2ebc6975e5eb2f8bf4d333f839b3b6a354
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-06 07:50:27 -0600

    adjustments

--- nvp-data-structures/src/index.js
+++ nvp-data-structures/src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { BrowserRouter, HashRouter  } from 'react-router-dom'
-const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter
+const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
+
 
 
 ReactDOM.render(
