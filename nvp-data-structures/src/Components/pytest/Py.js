6320fc48dc601cede462e3f119ab659f775d7263
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-06 07:51:31 -0600

    tinkering with pyscript

--- nvp-data-structures/src/Components/pytest/Py.js
+++ nvp-data-structures/src/Components/pytest/Py.js
@@ -5,8 +5,33 @@
 //         <h1>the div</h1>
 //         <div id="py1" ></div>   
 //         <py-script src="py" output="py1" >
+            
 //         </py-script>
 //     </div>)
 // }
 
 // export default Py
+
+// var perf =require('./Py.html');
+// import './Py.html'
+
+// const path = require('path');
+
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack'); //to access built-in plugins
+
+// module.exports = {
+//   module: {
+//     rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+//   },
+//   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+// };
+
+
+// const Py = (props) => {
+//       return (
+//          <iframe src={perf }></iframe>
+        
+//       );
+// }
+// export default Py;
