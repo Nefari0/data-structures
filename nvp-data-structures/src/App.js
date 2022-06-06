27f8dd3481dd7ebf4229db2dc533838aeae64705
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-06 07:47:40 -0600

    cleaning

--- nvp-data-structures/src/App.js
+++ nvp-data-structures/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import { HashRouter } from 'react-router-dom';
+import routes from './routes'
 import Auth from './Components/Auth/Auth'
 import Header from './Components/Header/Header';
 
 function App() {
   return (
+    <HashRouter>
     <div className="App">
       <Header/>
-      <Auth/>
+      {routes}
     </div>
+    </HashRouter>
   );
 }
 
