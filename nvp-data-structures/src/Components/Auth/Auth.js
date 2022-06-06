a5f83bcc87e467f421275b59d09196af3c5d1abe
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-06 07:42:22 -0600

    push session to Info.js. cleaning

--- nvp-data-structures/src/Components/Auth/Auth.js
+++ nvp-data-structures/src/Components/Auth/Auth.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react'
-// import a
 import './Auth.css'
 import { loginUser,logoutUser } from '../../redux/userReducer'
 import { connect } from 'react-redux'
-import axios from 'axios'
-import Info from '../Information/Info'
-import Loading from '../Loading/Loading'
-import { Route } from 'react-router-dom'
 
 class Auth extends Component {
 
@@ -20,40 +15,23 @@ class Auth extends Component {
             setPermission:true,
             isAuthenticated:false
         }
-        this.handleLogin = this.handleLogin.bind(this)
-        // this.login = this.login.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.resetState = this.resetState.bind(this)
-        // this.displayData = this.displayData.bind(this)
         this.thisLogout = this.thisLogout.bind(this)
     }
 
     componentDidMount(){
         const { user } = this.props
-        // this.props.updateCharacters()
     }
 
     componentDidUpdate(){
-        const { auth } = this.props.user.user
+        const { auth,id } = this.props.user.user
         const { isAuthenticated } = this.state
         if(auth === true && isAuthenticated === false) {
             this.setState({isAuthenticated:true})
+            this.props.history.push(`/info/${id}`)
         }
-        // console.log('auth',auth)
-        // console.lo g('characters',characters)
-        // const { user } = this.props
-        // const { setPermission } = this.state
-
-        // if(user.isLoggedIn === true && setPermission===true){
-        //     // this.setState({username:user.user.data.user,setPermission:false})
-        //     this.setState({username:user.user.user.user,isLoggedInState:user.isLoggedIn,setPermission:false})
-        // }
     }
-
-    //     componentDidUpdate(){
-    //     const { user } = this.props
-    //     console.log('this. props',this.props)
-    // }
 
     resetState(){
         this.setState({
@@ -68,29 +46,8 @@ class Auth extends Component {
         })
     }
     
-    handleLogin = async () => {
-        const { email,password } = this.state
-        // console.log('email',email)
-        // const { loginUser } = this.props
-        this.props.loginUser(email,password)
-        // axios.post('/api/auth/login',{email,password}).then(res => {
-            // this.setState({user:res.data})
-            // this.props.history.push("/info") // marked
-        // }).then(
-            // )
-
-        // this.resetState()
-        // this.displayData()
-    }
-
-    displayData = (params) => {
-        const { auth } = this.state.user
-        console.log('auth',auth)
-    }
-
-    // login(){
-    //     this.handleLogin()
-    // }
+    // handleLogin = async () => {
+    //     const { email,password } = this.state
 
     thisLogout() {
         this.setState({
@@ -105,30 +62,22 @@ class Auth extends Component {
         const { email,password } = this.state
 
         return(
-            // <div className="auth-container">
             <div>
-                { !auth === true ?
                <div className="auth-container">
-
-                {/* <img src={logo} alt='logo' /> */}
-
-                <h1 className='auth-title'>Data Structures</h1>
-                {this.state.errorMsg && <h3 className='auth-error-msg'>{this.state.errorMsg} <span onClick={this.closeErrorMessage}>X</span></h3>}
-                <div className='auth-input-box'>
-                    <p >Email:</p>
-                    <input placeholder={email} onChange={e => this.handleChange('email', e.target.value)} />
+                    <h1 className='auth-title'>Data Structures</h1>
+                    {this.state.errorMsg && <h3 className='auth-error-msg'>{this.state.errorMsg} <span onClick={this.closeErrorMessage}>X</span></h3>}
+                    <div className='auth-input-box'>
+                        <p >Email:</p>
+                        <input placeholder={email} onChange={e => this.handleChange('email', e.target.value)} />
+                    </div>
+                    <div className='auth-input-box'>
+                        <p >Password:</p>
+                        <input value={this.state.password} type='password' onChange={e => this.handleChange('password', e.target.value)} />
+                    </div>
+                    <div className='auth-button-container'>
+                        <button className='dark-button' onClick={() => this.props.loginUser(email,password)}> Login </button>
+                    </div>
                 </div>
-                <div className='auth-input-box'>
-                    <p >Password:</p>
-                    <input value={this.state.password} type='password' onChange={e => this.handleChange('password', e.target.value)} />
-                </div>
-                <div className='auth-button-container'>
-                    <button className='dark-button' onClick={() => this.handleLogin()}> Login </button>
-                    {/* <button className='dark-button' onClick={this.register}> Register </button> */}
-                </div>
-                </div>
-                : (<Route to="/info/" component={Info}/>)}
-                {/* : (<Info logout={this.thisLogout}/>)} */}
             </div>
         )
     }
@@ -138,6 +87,4 @@ function mapStateToProps(reduxState){
     return reduxState
 }
 
-// export default Auth
-// export default withRouter(connect(null, {loginUser})(Auth))
 export default connect(mapStateToProps, {loginUser,logoutUser})(Auth)
