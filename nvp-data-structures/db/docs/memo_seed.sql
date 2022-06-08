704cc3e10e1ad4cd495a89676889a4d4517655f3
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-07 22:25:08 -0600

    added seed. changed names to fit conventions

---
+++ nvp-data-structures/db/docs/memo_seed.sql
@@ -0,0 +1,12 @@
+
+-- CREATE TABLE data_memos (
+--   memo_id SERIAL PRIMARY KEY,
+--   title VARCHAR(250),
+--   body text
+-- )
+
+-- INSERT INTO ddata_memos (title,body)
+-- VALUES (
+--   'first memo',
+--   'this is the first memo written  for CDL data Structures'
+-- )
