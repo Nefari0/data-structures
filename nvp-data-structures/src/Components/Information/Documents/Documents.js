9fba01209435a84f13445f0d9e9823adb514d2ed
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-09 11:00:19 -0600

    add / edit / delete / view - using OneDoc.js

--- nvp-data-structures/src/Components/Information/Documents/Documents.js
+++ nvp-data-structures/src/Components/Information/Documents/Documents.js
@@ -7,6 +7,7 @@ const Documents = (props) => {
 
     const [docs,setDocs] = useState([]) // -- All docs in DB
     const [isLoading,setIsLoading] = useState(false)
+    const [create,setCreate] = useState(false) // -- Create new document
     const [selected,setSelected] = useState(null) // -- For displaying currently selected document
     const currentDoc = localStorage['currentDoc'] // -- For auto loading currently selected document
 
@@ -18,17 +19,40 @@ const Documents = (props) => {
         selectMemo(currentDoc)
     },[])
 
-        // -- Get all docs - Reset current doc if param === true -- //
+// ------------ DB managment ------------------ //
+    // -- Create new document -- //
+    const newDoc = (state) => {
+        setIsLoading(true)
+        axios.post('/api/memos/new',state).then(res => {
+            selectMemo(res.data.memo_id)
+            setCreate(false)
+            setIsLoading(false)
+            grabDocs()
+        })
+    }
+    // -- Update document -- //
+    const sendUpdate = (state) => {
+        setIsLoading(true)
+        axios.put('/api/memos/update', state).then(() => setIsLoading(false))
+    }
+    // -- Get all docs - Reset current doc if param === true -- //
     const grabDocs = async (clearCurrent) => { 
-
         if(clearCurrent){selectMemo('null')}
-
         await setIsLoading(true)
         await axios.get('/api/memos/get').then(res => {
             setDocs(res.data)
             setIsLoading(false)
         }).catch(err => console.log('error',err))
     }
+    // -- Delete document -- //
+    const deleteDoc = (state) => {
+        setIsLoading(true)
+        axios.delete(`/api/memos/delete/${state.memo_id}`).then(() => {
+            grabDocs(true)
+            setIsLoading(false)
+        })
+    }
+// --------------------------------------------------- //
 
     // -- Select doc to open -- //
     const selectMemo = (id) => {
@@ -39,7 +63,7 @@ const Documents = (props) => {
     // -- Find & Display Currently Selected Doc -- //
     const currentItem = docs.filter(docEl => docEl.memo_id === parseInt(selected)) 
     const mappedItem = currentItem.map(el => {
-        return <OneDoc key={el.memo_id} body={el.body} memo_id={el.memo_id} title={el.title} selectMemo={selectMemo} />
+        return <OneDoc key={el.memo_id} body={el.body} memo_id={el.memo_id} title={el.title} selectMemo={selectMemo} DB={sendUpdate} isLoading={isLoading} deleteDoc={deleteDoc} />
     })
 
     // -- List of all Docs in DB -- //
@@ -50,13 +74,21 @@ const Documents = (props) => {
     return(
         <div className="display-matrix">
             <section className="search-bar" style={{margin:'0px'}}>
+
                 {isLoading ? <Loading/> : null}
+
                 <a onClick={() => grabDocs(true)}>reload all</a>
+
+                {selected === 'null' ? <a onClick={() => setCreate(!create)} >{!create ? 'new' : 'cancel'}</a> : null}
+
                 <a onClick={() => props.handleForm('currentView','')}>close</a>
+
             </section>
     
             <section className="stats-container">
-                {selected === 'null' ? mappedDocList : mappedItem}
+
+                {!create ? (selected === 'null' ? mappedDocList : mappedItem) : <OneDoc body={''} memo_id={null} title={''}  selectMemo={selectMemo} DB={newDoc} isLoading={isLoading} />}
+
             </section>
         </div>
     )
