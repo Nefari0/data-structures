1fbbb7e70c3b7ba2b2d8ea631a73ad5e0e09d885
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-09 10:58:37 -0600

    Modularity. Used to add / view / edit / delete

--- nvp-data-structures/src/Components/Information/Documents/OneDoc.js
+++ nvp-data-structures/src/Components/Information/Documents/OneDoc.js
@@ -5,7 +5,6 @@ import axios from 'axios'
 const OneDoc = (props) => {
 
     const { body,title,memo_id } = props
-    var [ sending,setSending ] = useState(false)
     var [ state,setState ] = useState({
         body:body,
         title:title,
@@ -19,21 +18,17 @@ const OneDoc = (props) => {
             [prop]: event.target.value,
         }));
     };
-    
-    const sendUpdate = () => {
-        const { body,memo_id,title } = state
-        setSending(true)
-        axios.post('/api/memo/update', {body,memo_id,title}).then(res => {
-            setSending(false)
-        })
+
+    const handleDB = (state) => {
+        props.DB(state)
     }
 
     return(
     <form className="admin-memo-body">
         <header className='data-spec' >
             <strong onClick={() => props.selectMemo('null')}>close</strong>
-            <strong onClick={() => setState('body',props.body)}>reset</strong>
-            <strong onClick={() => sendUpdate()} >{sending === true ? 'saving...' : 'save'}</strong>
+            {!props.memo_id ? null :<strong onClick={() => props.deleteDoc(state)}>delete</strong>}
+            <strong onClick={() => handleDB(state)} >{props.isLoading ? 'saving...' : 'save'}</strong>
         </header>
         <textarea value={state.title} onChange={(e) => input('title',e)} ></textarea>
         <textarea value={state.body} onChange={(e) => input('body',e)} rows="10"  > </textarea>
