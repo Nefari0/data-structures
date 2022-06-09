b737c2939e167515e1059c4af8ab302994b3c278
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-09 10:53:33 -0600

    add / delete

--- nvp-data-structures/server/controllers/docsController.js
+++ nvp-data-structures/server/controllers/docsController.js
@@ -1,5 +1,10 @@
 module.exports = {
-    newMemo: async (req,res) => {},
+    newMemo: async (req,res) => {
+        const db = req.app.get('db')
+        const { body,title } = req.body
+        const memo = await db.docs.new_memo([body,title])
+        return res.status(200).send(memo[0])
+    },
 
     getDocs: async (req,res) => {
         const db = req.app.get('db')
@@ -14,5 +19,10 @@ module.exports = {
         return res.status(200).send(memo)
     },
 
-    deleteMemo: async (req,res) => {}
+    deleteMemo: async (req,res) => {
+        const { memo_id } = req.params
+        const db = req.app.get('db')
+        const memo = await db.docs.delete_memo([memo_id])
+        return res.status(200).send(memo)
+    }
 }
