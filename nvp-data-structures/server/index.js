8bab0de4c2f33b8e3e6b0104d3afaffdb26d8f53
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-09 10:52:48 -0600

    add / delete docs endpoints. optimazing

--- nvp-data-structures/server/index.js
+++ nvp-data-structures/server/index.js
@@ -4,12 +4,10 @@ const session = require('express-session')
 const massive = require('massive')
 const authController = require('./controllers/authController');
 const cancerStatController = require('./controllers/cancerStatController');
-const dataTestController = require('./controllers/dataTestController');
 const employeeController = require('./controllers/employeeController');
 const titanicController = require('./controllers/titanicController');
 const docsController = require('./controllers/docsController')
 const path = require('path');
-// const docsController = require('./controllers/docsController');
 
 const { SESSION_SECRET, CONNECTION_STRING, SERVER_PORT } = process.env;
 
@@ -22,11 +20,11 @@ app.use(
             resave: false,
             saveUninitialized: true,
             secret: SESSION_SECRET,
-            cookie: { maxAge: 1000 * 60 * 5 },
+            cookie: { maxAge: 1000 * 60 * 60 * 24 },
         }),
     )
 
-// user end points
+// --- user / auth end points --- //
 app.post('/api/auth/register', authController.register)
 app.post('/api/auth/login', authController.login)
 app.get('/api/auth/logout', authController.logout)
@@ -34,25 +32,20 @@ app.post('/api/auth/browser/login', authController.browserLogin)
 
 // --- memos / docs endpoinds --- //
 app.get('/api/memos/get',docsController.getDocs)
-app.post('/api/memo/update',docsController.editMemo)
+app.put('/api/memos/update',docsController.editMemo)
+app.post('/api/memos/new',docsController.newMemo)
+app.delete('/api/memos/delete/:memo_id',docsController.deleteMemo)
 
-// test data endpoints
-app.get('/api/testdata/all', dataTestController.getData)
-// app.get('/api/projects/all', dataTestController.getData)
-
-// cancer stats endpoints
+// --- cancer stats endpoints --- //
 app.get('/api/cancer/all', cancerStatController.getAllStats)
 app.post('/api/cancer/add',cancerStatController.addToDatabase)
 app.post('/api/cancer/transceive',cancerStatController.oneResult)
 // app.get('/api/cancer/result',cancerStatController.oneResult)
 
-// breaking bad endpoints
-// app.get('/api/bb/all')
-
-// employee endpoints
+// --- employee endpoints --- //
 app.get('/api/employees/all',employeeController.getAllEmployees)
 
-// titanic end points
+// --- titanic end points --- //
 app.get('/api/passengers/all', titanicController.getAll)
 
 // -----server ------
