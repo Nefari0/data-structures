695cde50f196064ae683139107e5d9418e988450
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-17 21:21:33 -0600

    serviceWorker

--- nvp-data-structures/src/index.js
+++ nvp-data-structures/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { BrowserRouter, HashRouter  } from 'react-router-dom'
+import * as serviceWorker from './serviceWorker';
 const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 
 
@@ -24,4 +25,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.register();
 reportWebVitals();
