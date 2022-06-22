1bf4130600cb548b5e6d09edbe9402b7189b2738
Nefari0 <chrisdemontigny@yahoo.com>
2022-06-22 09:46:42 -0600

    added text  encryption

--- nvp-data-structures/server/controllers/docsController.js
+++ nvp-data-structures/server/controllers/docsController.js
@@ -1,7 +1,10 @@
+const cryptKeys = require('../middleware/cryptKeys')
+
 module.exports = {
     newMemo: async (req,res) => {
         const db = req.app.get('db')
-        const { body,title,category } = req.body
+        const { title,category } = req.body
+        var body = cryptKeys.encrypt(req.body.body)
         const memo = await db.docs.new_memo([body,title,category])
         return res.status(200).send(memo[0])
     },
@@ -9,12 +12,14 @@ module.exports = {
     getDocs: async (req,res) => {
         const db = req.app.get('db')
         const docs = await db.docs.get_all_memos()
+        cryptKeys.decrypt(docs)
         return res.status(200).send(docs)
     },
 
     editMemo: async (req,res) => {
-        const { memo_id,body,title,category } = req.body
+        const { memo_id,title,category } = req.body
         const db = req.app.get('db')
+        var body = cryptKeys.encrypt(req.body.body)
         const memo = await db.docs.edit_memo([body,title,category,memo_id])
         return res.status(200).send(memo)
     },
@@ -24,5 +29,24 @@ module.exports = {
         const db = req.app.get('db')
         const memo = await db.docs.delete_memo([memo_id])
         return res.status(200).send(memo)
+    },
+
+    // --- Encrypt all items - this is not a toy --- //
+    encryptAll: async (req,res) => {
+        const db = req.app.get('db')
+        const docs = await db.docs.get_all_memos()
+        var newDocs = cryptKeys.encryptAllText(docs)
+
+        const addToDb = async (input) => {
+            const { body,title,category,memo_id } = input
+            const memo = await db.docs.edit_memo([body,title,category,memo_id])
+            return
+        }
+
+        newDocs.forEach(element => {
+            addToDb(element)
+        });
+
+        return
     }
 }
